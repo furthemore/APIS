70b3cf7efa730333ff99cb685066eb6a801d7b04
root <root@ubuntu.members.linode.com>
2016-07-29 00:54:43 +0000

    Added department list to views

--- registration/views.py
+++ registration/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import json
+
 
 from .models import *
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Registration Index")
+    context = {}
+    return render(request, 'registration/registration-form.html', context)
 
+
+###################################
+# Utilities
+
+def getDepartments(request):
+    depts = Department.objects.get(volunteerListOk=True)
+    data = json.dumps(depts)
+    return HttpResponse(data, content_type='application/json')
