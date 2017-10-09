73e0d834c05e76d0766d985c78eead6ccab41e02
Yuukari Shishome <yuuisim@gmail.com>
2017-10-09 16:46:28 -0400

    Upgrade to Django 1.11.6, Merge Events and Volunteers apps

--- fm_eventmanager/urls.py
+++ fm_eventmanager/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^registration/', include('registration.urls')),
+    url(r'^backend/events/', include('events.urls')),
+    url(r'^backend/volunteer/', include('volunteer.urls')),
     url(r'^admin/', admin.site.urls),
 ]
