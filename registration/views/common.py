db4478696961e64b3e9bdbde9e08fa1e7be7a0e8
Rechner Fox <rechner@users.noreply.github.com>
2020-02-07 20:56:11 -0800

    Fix email import references in admin.
    Resolves https://sentry.io/share/issue/17764dada4ab4d35816a007e2933c108/

--- registration/views/common.py
+++ registration/views/common.py
@@ -618,7 +618,7 @@ def checkout(request):
 
         clear_session(request)
         try:
-            registration.emails.sendRegistrationEmail(order, order.billingEmail)
+            registration.emails.send_registration_email(order, order.billingEmail)
         except Exception as e:
             logger.error("Error sending RegistrationEmail - zero sum.")
             logger.exception(e)
@@ -682,7 +682,7 @@ def checkout(request):
         cartItems.delete()
         clear_session(request)
         try:
-            registration.emails.sendRegistrationEmail(order, order.billingEmail)
+            registration.emails.send_registration_email(order, order.billingEmail)
         except Exception as e:
             event = Event.objects.get(default=True)
             registrationEmail = getRegistrationEmail(event)
