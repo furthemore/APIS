cb7fc33faa5aa18b2274e9e3e57f426e2979f110
rechner <jkltechinc@gmail.com>
2020-02-14 16:26:17 -0800

    Preliminary influxDB backend for cron metrics
    - Add library dependancy to requriements
    - Update example configuration files

--- registration/management/commands/cron_metrics.py
+++ registration/management/commands/cron_metrics.py
@@ -1,12 +1,17 @@
+import datetime
+from abc import ABCMeta, abstractmethod
+
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
+from influxdb import InfluxDBClient
 
 from registration.models import *
 
 
 """
 There's a few options for where these metrics could end up.  These should eventually be configurable:
- - built-in database metrics table: (simplest, no external dependancy).
+ - built-in database metrics table: (simplest, no external dependency).
  - prometheus: Time-series database, with existing reporters for e.g. django request processors.
  - influxdb: Another great time-series database.
 """
@@ -55,10 +60,73 @@ class Command(BaseCommand):
             )
             active_staff_count = active_staff.count()
 
+            backend_class = eval(settings.APIS_METRICS_BACKEND)
+            try:
+                backend_settings = settings.APIS_METRICS_SETTINGS
+            except AttributeError:
+                backend_settings = {}
+            backend = backend_class(backend_settings)
+
             print "Event: {0} - (total: {1})".format(event, total_count)
             for level in price_levels:
                 print level, "-", level_bins[level.id]
+                backend.write(event, level, level_bins[level.id])
             print "Staff: {0} ({1} active)".format(
                 total_staff_count, active_staff_count
             )
+            backend.write(event, "_staff_total", total_staff_count)
+            backend.write(event, "_staff_active", active_staff_count)
             print ("==========")
+
+
+class CronReporterABC:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(selfself, config):
+        pass
+
+    @abstractmethod
+    def write(self, event, topic, value):
+        pass
+
+
+class DummyReporter(CronReporterABC):
+    def __init__(self, config):
+        pass
+
+    def write(self, event, topic, value):
+        pass
+
+
+CronReporterABC.register(DummyReporter)
+
+
+class InfluxDBReporter(CronReporterABC):
+    def __init__(self, config):
+        """
+        Statistics collecting backend for recording to InfluxDB
+
+        :param settings: dictionary of InfluxDB-specific client parameters
+               (see https://influxdb-python.readthedocs.io/en/latest/api-documentation.html#influxdbclient)
+        """
+        self.client = InfluxDBClient(*config)
+        db_name = settings.get("database")
+        if db_name:
+            self.client.create_database(db_name)
+
+    def write(self, event, topic, value):
+        now = datetime.datetime.utcnow()
+        timestamp = now.isoformat("T") + "Z"
+
+        json_body = {
+            "measurement": topic,
+            "tags": {"event": event,},
+            "time": timestamp,
+            "fields": {"Int_value": value,},
+        }
+
+        self.client.write(json_body)
+
+
+CronReporterABC.register(InfluxDBReporter)
