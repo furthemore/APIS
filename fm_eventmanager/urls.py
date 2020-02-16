4c1f5ab8e3f43403a219d8ac07d477e5de7e5311
Rechner Fox <rechner@users.noreply.github.com>
2020-02-16 07:21:27 -0800

    Rechner/maintenance mode
    * Add maintenance mode plugin (toggle with `./manage.py maintenance_mode [on|off]`)
    * Excludes u2f from maintenance mode
    * Add URLs to set maintenance mode.
    * redirect root to registration landing page.

--- fm_eventmanager/urls.py
+++ fm_eventmanager/urls.py
@@ -2,13 +2,16 @@ import django_u2f.urls
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = [
+    url(r"^$", RedirectView.as_view(url="registration")),
     url(r"^registration/", include("registration.urls", namespace="registration")),
     url(r"^admin/", admin.site.urls),
     url(r"^u2f/", include(django_u2f.urls, namespace="u2f")),
+    url(r"^maintenance-mode/", include("maintenance_mode.urls")),
 ]
 
 if settings.DEBUG:
