2024ca424fc03083880f6580d9308805610b20bd
meanderfox <meanderfox@users.noreply.github.com>
2021-12-12 01:32:18 -0500

    beefed up tests

--- registration/tests/test_emails.py
+++ registration/tests/test_emails.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from mock import patch
 
-from registration.models import Event, Badge, Attendee, TempToken
+from registration.models import Event, Badge, Staff, Attendee, TempToken, Order
 from registration import emails
 from registration.tests.common import DEFAULT_EVENT_ARGS
 
@@ -27,8 +27,44 @@ class TestStaffEmails(TestCase):
         self.badge = Badge(attendee=self.attendee, event=self.event, badgeName="DisStaff")
         self.badge.save()
         self.token = TempToken(email=self.attendee.email, validUntil="2048-12-12")
+        self.staff = Staff(attendee=self.attendee, event=self.event)
+        self.staff.save()
+        self.order = Order(
+            total=60,
+            reference="HUGBUG",
+            billingEmail=self.attendee.email
+        )
+        self.order.save()
 
     @patch("registration.emails.sendEmail")
     def test_send_new_staff_email(self, mock_sendEmail):
         emails.send_new_staff_email(self.token)
         mock_sendEmail.assert_called_once()
+        recipients = mock_sendEmail.call_args[0][1]
+        plainText = mock_sendEmail.call_args[0][3]
+        htmlText = mock_sendEmail.call_args[0][4]
+        self.assertIn(self.token.token, plainText)
+        self.assertIn(self.token.token, htmlText)
+        self.assertEqual(recipients, [self.attendee.email])
+
+    @patch("registration.emails.sendEmail")
+    def test_send_staff_promotion_email(self, mock_sendEmail):
+        emails.send_staff_promotion_email(self.staff)
+        mock_sendEmail.assert_called_once()
+        recipients = mock_sendEmail.call_args[0][1]
+        plainText = mock_sendEmail.call_args[0][3]
+        htmlText = mock_sendEmail.call_args[0][4]
+        self.assertIn(self.staff.registrationToken, plainText)
+        self.assertIn(self.staff.registrationToken, htmlText)
+        self.assertEqual(recipients, [self.attendee.email])
+
+    @patch("registration.emails.sendEmail")
+    def test_sendStaffRegistrationEmail(self, mock_sendEmail):
+        emails.sendStaffRegistrationEmail(self.order.pk)
+        mock_sendEmail.assert_called_once()
+        recipients = mock_sendEmail.call_args[0][1]
+        plainText = mock_sendEmail.call_args[0][3]
+        htmlText = mock_sendEmail.call_args[0][4]
+        self.assertIn(self.order.reference, plainText)
+        self.assertIn(self.order.reference, htmlText)
+        self.assertEqual(recipients, [self.attendee.email])
