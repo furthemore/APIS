1263faf9ab7edd8cd7ee3af1193f858cb1e646c2
meanderfox <meanderfox@users.noreply.github.com>
2021-12-20 01:07:42 -0500

    added more model tests

--- registration/tests/test_model.py
+++ registration/tests/test_model.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from decimal import Decimal
 
-from registration.models import Venue
+from registration.models import Charity, Venue
 from registration.tests.common import DEFAULT_VENUE_ARGS
+
+
+class TestCharity(TestCase):
+    def setUp(self):
+        self.charity = Charity(name="Reconstructing the Past", donations=5.00)
+        self.charity.save()
+
+    def test_charity_fields(self):
+        self.assertIsInstance(self.charity.name, str)
+        self.assertIsInstance(self.charity.url, str)
+        self.assertIsInstance(self.charity.donations, float)
+        self.assertEquals(str(self.charity), self.charity.name)
 
 
 class TestVenue(TestCase):
@@ -10,4 +23,9 @@ class TestVenue(TestCase):
         self.venue.save()
 
     def test_venue_fields(self):
+        self.assertIsInstance(self.venue.name, str)
+        self.assertIsInstance(self.venue.address, str)
+        self.assertIsInstance(self.venue.city, str)
+        self.assertIsInstance(self.venue.state, str)
+        self.assertIsInstance(self.venue.country, str)
         self.assertEquals(str(self.venue), self.venue.name)
