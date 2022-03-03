88e899d99f520ffa9fa46876b5d3677bb3bfb4b3
Dozer Trashpanda <99625007+DozerTrashpanda@users.noreply.github.com>
2022-03-02 21:19:58 -0500

    add badge name to dealer export (#228)
    * add badge name to dealer export
    
    * add badge name to dealer and dealer assistant exports
    
    Co-authored-by: meanderfox <meanderfox@users.noreply.github.com>

--- registration/admin.py
+++ registration/admin.py
@@ -254,11 +254,14 @@ class DealerAsstInline(NestedTabularInline):
 
 
 class DealerAsstResource(resources.ModelResource):
+    badgeName = fields.Field()
+
     class Meta:
         model = DealerAsst
         fields = (
             "id",
             "name",
+            "badgeName",
             "email",
             "license",
             "event__name",
@@ -267,6 +270,24 @@ class DealerAsstResource(resources.ModelResource):
             "dealer__approved",
             "dealer__tableNumber",
         )
+        export_order = (
+            "id",
+            "name",
+            "badgeName",
+            "email",
+            "license",
+            "event__name",
+            "dealer__businessName",
+            "dealer__attendee__email",
+            "dealer__approved",
+            "dealer__tableNumber",
+        )
+
+    def dehydrate_badgeName(self, obj):
+        badge = Badge.objects.filter(attendee=obj.attendee, event=obj.event).last()
+        if badge is None:
+            return "--"
+        return badge.badgeName
 
 
 def send_assistant_registration_email(modeladmin, request, queryset):
@@ -330,11 +351,14 @@ admin.site.register(DealerAsst, DealerAsstAdmin)
 
 
 class DealerResource(resources.ModelResource):
+    badgeName = fields.Field()
+
     class Meta:
         model = Dealer
         fields = (
             "id",
             "event__name",
+            "badgeName",
             "attendee__firstName",
             "attendee__lastName",
             "attendee__address1",
@@ -373,6 +397,7 @@ class DealerResource(resources.ModelResource):
         export_order = (
             "id",
             "event__name",
+            "badgeName",
             "attendee__firstName",
             "attendee__lastName",
             "attendee__address1",
@@ -408,6 +433,12 @@ class DealerResource(resources.ModelResource):
             "discountReason",
             "emailed",
         )
+
+    def dehydrate_badgeName(self, obj):
+        badge = Badge.objects.filter(attendee=obj.attendee, event=obj.event).last()
+        if badge is None:
+            return "--"
+        return badge.badgeName
 
 
 class DealerAdmin(NestedModelAdmin, ImportExportModelAdmin):
