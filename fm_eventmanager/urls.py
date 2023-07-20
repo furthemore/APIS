b89b8557a32c780d6168486ae0d679ed8d27e16a
meanderfox <meanderfox@users.noreply.github.com>
2023-07-20 13:10:08 -0400

    add quick and dirty robots.txt (#297)
    * Update urls.py
    
    * Update test_index.py

--- fm_eventmanager/urls.py
+++ fm_eventmanager/urls.py
@@ -3,10 +3,16 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.http import HttpResponse
 
 admin.autodiscover()
 
 urlpatterns = [
+    url(
+        r"robots.txt",
+        lambda x: HttpResponse("User-Agent: *\n\nDisallow: /", content_type="text/plain"),
+        name="robots_file"
+    ),
     url(r"^registration/", include("registration.urls", namespace="registration")),
     url(r"^admin/", admin.site.urls),
     url(r"^u2f/", include(django_u2f.urls, namespace="u2f")),
